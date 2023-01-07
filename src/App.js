882029cc27fffeff07cfffbcfb50ccf52e4d8b73
Shubham Tripathi <shubhhtr@gmail.com>
2023-01-07 23:50:28 +0530

    added nav-bar and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import Nav from './Nav.js';
-import {BrowserRouter} from "react-router-dom"
+import Nav from './Components/Nav.js';
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 import './App.css';
+import Footer from "./Components/Footer.js";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Nav />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={ <h1>Home Page</h1> } />
+          <Route path="/add" element={ <h1>Add Page</h1> } />
+          <Route path="/update" element={ <h1>Update Page</h1> } />
+          <Route path="/logout" element={ <h1>Logout Page</h1> } />
+          <Route path="/profile" element={ <h1>Profile Page</h1> } />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </>
+    
   );
 }
 
---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer=()=>{
+    return (
+        <div className="footer">
+            Footer <br />
+            <p>This is a shopping website based on Nodejs and Reactjs <hr />
+                 Created by : Shubham Tripathi</p>
+        </div>
+    )
+}
+
+export default Footer;
